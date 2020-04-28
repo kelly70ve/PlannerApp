f79a784d9be830c9f59a79e71528df2976b20170
Kelly Gowing <Gowing.kelly@yahoo.com>
2020-04-27 20:23:19 -0700

    added psuedocode for js

---
+++ Develop/script.js
@@ -0,0 +1,5 @@
+// load day and time at top of page
+// fill in times from 6AM to 6PM
+// dynamically add classes to times (past, present, future)
+// save newly inputed information
+// localStorage saved info
