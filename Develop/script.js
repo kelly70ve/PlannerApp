784b87c4ad91b65dcdf9e28dadc72a3e9a00e5d3
Kelly Gowing <Gowing.kelly@yahoo.com>
2020-05-01 21:18:31 -0700

    finally got moment to work

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,41 @@
-// load day and time at top of page
+
+
+$(document).ready(function(){
+  var today = moment().format('dddd, MMMM Do');
+  
+  // load day at top of page
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
+
 // fill in times from 6AM to 6PM
 // dynamically add classes to times (past, present, future)
 // save newly inputed information
 // localStorage saved info
+
+
+
+
+
+
+
+/* <div class="row time-block">
+<div class="col-1 hour">
+9AM
+</div>
+<textarea class="col-10 past description"></textarea>
+<button class="col-1 saveBtn d-flex justify-content-center">
+  <i class="fas fa-save"></i>
+</button>
+</div>    */
--- README copy.md
+++ README copy.md
@@ -42,5 +42,8 @@ You are required to submit the following for review:
 
 * The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.
 
+
+- [Moment Cheat Sheet](https://devhints.io/moment)
+
 - - -
 © 2019 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved.
