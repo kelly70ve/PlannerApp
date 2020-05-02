b47e8db641dcdc8fabc9e263be8066e5a1e09307
Kelly Gowing <Gowing.kelly@yahoo.com>
2020-05-02 15:33:02 -0700

    save tasks to local storage

--- Develop/script.js
+++ Develop/script.js
@@ -2,27 +2,28 @@
 $(document).ready(function () {
   var today = moment().format('dddd, MMMM Do');
   var currentHour = parseInt(moment().format("HH"));
+  var workDay = 12;
+  var ppf = ""
+  var savedTasks = [];
 
+  // Initialize page: 
   // load day at top of page
   $("#currentDay").text(today);
 
-  // fill in times from 6AM to 6PM
-
-  var workDay = 12;
+  // Load Time Blocks
+  loadTimes();
 
   // Create Time Blocks
   function loadTimes() {
     for (var i = 0; i < workDay + 1; i++) {
-      // Get hour 
+      // Get hours 
       var hour = moment().hour(6);
       hour = hour.add(i, 'h')
 
       var hourString = hour.format("h A");
       var hourNum = parseInt(hour.format("HH"));
 
-      var ppf = ""
-
-      // Create an if statement that checks if current hour is past, present, future
+      // Checks if current hour relative to hour blocks is past, present, future
 
       if (currentHour > hourNum){
         ppf = "past"
@@ -34,20 +35,35 @@ $(document).ready(function () {
         ppf = "present"
       }
 
-      // Create hour El 
+      // Create visual componets with bootstrap styling 
       var rowEl = $("<div>").addClass("row time-block");
       var hourEl = $("<div>").addClass("pt-3 col-1 hour d-flex justify-content-center").text(hourString);
-      // will need to dynamically fill past or future *** 
-      var textEl = $("<textarea>").addClass("col-9 description " + ppf);
-      var buttonEl = $("<button>").addClass("col-1 saveBtn d-flex justify-content-center").append($("<i>").addClass("fas fa-save"));
+      var textEl = $("<textarea>").addClass("col-9 description " + ppf).attr("data-hour", hourString);
+      var buttonEl = $("<button>").addClass("col-1 saveBtn d-flex justify-content-center").attr("data-hour", hourString).append($("<i>").addClass("fas fa-save"));
 
       // Append Elements
       $("#time-container").append(rowEl.append(hourEl, textEl, buttonEl));
     }
   }
 
-  loadTimes();
+  // Save inputs to localStorage on click 
+  $(".saveBtn").on("click", function() {
+    event.preventDefault();
+    var thisHour = $(this).attr("data-hour")
+    var task = $(`.description[data-hour='${thisHour}'`).val().trim();
 
+    var hourTask = {
+      saveHour: thisHour,
+      saveTask: task
+    }
+    savedTasks.push(hourTask)
+    localStorage.setItem("tasks", JSON.stringify(savedTasks))
+  });
+
+  // initialize
+  // funcion init() {
+  //   var storedSavedTasks
+  // }
 
 
 
@@ -60,7 +76,5 @@ $(document).ready(function () {
 
 });
 
-
-// dynamically add classes to times (past, present, future)
 // save newly inputed information
 // localStorage saved info
